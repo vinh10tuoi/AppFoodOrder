bca1cf0973ba77fe76de9cbc53bc731fd119de5c
vinh10tuoi <87126237+vinh10tuoi@users.noreply.github.com>
2024-04-11 02:16:35 +0700

    update

---
+++ app/src/main/java/com/example/apporderfood/adapters/HomeVerAdapter.java
@@ -0,0 +1,62 @@
+package com.example.apporderfood.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.apporderfood.R;
+import com.example.apporderfood.models.HomeVerModel;
+
+import java.util.List;
+
+public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHolder> {
+
+
+
+
+
+    public HomeVerAdapter(Context context, List<HomeVerModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    Context context;
+    List<HomeVerModel> list;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_vertical_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(list.get(position).getImage());
+        holder.name.setText(list.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name,timing,rating,price;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.ver_img);
+            name = itemView.findViewById(R.id.name);
+            timing = itemView.findViewById(R.id.timing);
+            rating = itemView.findViewById(R.id.rating);
+            price = itemView.findViewById(R.id.price);
+        }
+    }
+}
--- app/src/main/java/com/example/apporderfood/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/apporderfood/ui/home/HomeFragment.java
@@ -7,11 +7,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.apporderfood.R;
+import com.example.apporderfood.adapters.HomeHorAdapter;
+import com.example.apporderfood.adapters.HomeVerAdapter;
 import com.example.apporderfood.databinding.FragmentHomeBinding;
+import com.example.apporderfood.models.HomeHorModel;
+import com.example.apporderfood.models.HomeVerModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+
+    RecyclerView homeHorizontalRec, homeVerticalRec;
+    List<HomeHorModel> homeHorModelList;
+    HomeHorAdapter homeHorAdapter;
+
+    ///Vertical Layout
+    List<HomeVerModel> homeVerModelList;
+    HomeVerAdapter homeVerAdapter;
 
     private FragmentHomeBinding binding;
 
@@ -20,6 +38,37 @@ public class HomeFragment extends Fragment {
 
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeVerticalRec = root.findViewById(R.id.home_ver_rec);
+
+        //////////HorizonRec RecycleView
+        homeHorModelList = new ArrayList<>();
+
+        homeHorModelList.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.hamburger,"Bơ gơ"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.fried_potatoes,"Khoai tây"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.ice_cream,"Kem"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.sandwich,"Sandwich"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(),homeHorModelList);
+        homeHorizontalRec.setAdapter(homeHorAdapter);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
+
+        //////////VerticalRec RecycleView
+        homeVerModelList = new ArrayList<>();
+
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza1,"Pizza","9:00 đến 22:00","5.0","Giá : 35000VND"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza2,"Pizza","9:00 đến 22:00","5.0","Giá : 35000VND"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza3,"Pizza","9:00 đến 22:00","5.0","Giá : 35000VND"));
+
+        homeVerAdapter = new HomeVerAdapter(getActivity(),homeVerModelList);
+        homeVerticalRec.setAdapter(homeVerAdapter);
+        homeVerticalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        homeVerticalRec.setHasFixedSize(true);
+        homeVerticalRec.setNestedScrollingEnabled(false);
 
         return root;
     }
